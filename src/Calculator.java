80345082ca69222a2b801f4027228007ee20e62c
user <user@user.com>
2022-02-28 23:48:30 +0100

    showMsg to secondBranch

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,6 +2,6 @@ public class Calculator {
 
     public void showMsg()
     {
-        System.out.println("Hello from the new Branch");
+        System.out.println("Hello from the second Branch");
     }
 }

ca0d8d5cb5f79fa3faa3bb85a76e7771d86fdf09
user <user@user.com>
2022-02-28 23:55:43 +0100

    test Conflict

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,6 +2,6 @@ public class Calculator {
 
     public void showMsg()
     {
-        System.out.println("Hello from the second Branch");
+        System.out.println("Hello from the Branch");
     }
 }
